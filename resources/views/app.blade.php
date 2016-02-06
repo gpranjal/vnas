5c72752ada705bfdaa0b77b849032736aae4a3c2
Zach Heath <zheath@unomaha.edu>
2016-02-06 15:08:18 -0600

    Commit .htaccs and URL updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -53,7 +53,17 @@
 		</div>
 	</nav>
 
-	@yield('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+
+					@yield('content')
+
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
