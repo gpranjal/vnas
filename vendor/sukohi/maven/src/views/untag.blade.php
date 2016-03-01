9f7d113d4417ea07fde0f837245bfb9803e2f2dd
fmohammed1022 <fmohammed@unomaha.edu>
2016-02-29 19:21:11 -0600

    Farhan: Testing some more CSS changes.

--- vendor/sukohi/maven/src/views/untag.blade.php
+++ vendor/sukohi/maven/src/views/untag.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <div class="col-md-8 col-md-offset-2">
+<div class="container">
+    <div class="col-md-8 col-md-offset-1">
         <div class="panel panel-default">
             <div class="panel-heading"> <!-- This div has the orange color for the VNA-->
                 <h4>FAQ</h4>
