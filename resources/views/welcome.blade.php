635c51e44eac4e227afcea945371cb936ad38705
fmohammed1022 <fmohammed@unomaha.edu>
2016-03-05 09:47:46 -0600

    Farhan: Testing the grid classes in app.blade to fix width issues.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,16 @@
 					The Face of Care
 				</h3>
             </div>
-	            <div class="panel-body">
-	                <img src="{{ asset('img/brandmark_main.png') }}" class="img-responsive img-thumbnail" alt="Responsive image">
-	             </div>
-
 	            <div class="panel-body" align="center">
-	            <h4>Serving Omaha, Council Bluffs, and Surrounding Communities</h4>
+	                <img src="{{ asset('img/brandmark_main.png') }}" class="img-responsive	" alt="Responsive image">
+					<br>
+	            	<h4>Serving Omaha, Council Bluffs, and Surrounding Communities</h4>
+					<br>
+
+
 	            	<a name="callButton" class="btn btn-default btn-primary"  href="tel:402-930-4240" role="button" align="center">Contact Us</a>
+					<br>
+
 
 					{{--<a class="btn btn-default" href="tel:402-930-4240" role="button"><img src="{{ asset('img/call.png') }}"></a>--}}
 
@@ -26,6 +29,8 @@
 						<input class="btn btn-default btn-success" type="submit" value="Donate to VNA"  border="0" name="submit" alt="Donate to VNA">
 						<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 					</form>
+
+					<br>
 
  	         		{{--<button class="btn btn-default btn-success" align="center" type="button" value="Donate To VNA">Donate to VNA</button>--}}
 	         	</div>
