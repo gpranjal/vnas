c1b2b61e06747ab262ecc69242cd92201b36ee8c
Zach Heath <zheath@unomaha.edu>
2016-03-05 15:29:17 -0600

    Zach - Take only 1 user record

--- app/Http/Controllers/VnasUsersController.php
+++ app/Http/Controllers/VnasUsersController.php
@@ -29,12 +29,16 @@ class VnasUsersController extends Controller {
 
             if( $isCareGiver != "" )
             {
-                $vnas_users = Vnas_record::where( 'caregiver_id' , '=' , $isCareGiver )->get( array('id','caregiver_id','caregiver_fname','caregiver_lname','caregiver_phone','caregiver_mob'));
+                $vnas_users = Vnas_record::where( 'caregiver_id' , '=' , $isCareGiver )
+                    ->take(1)
+                    ->get( array('id','caregiver_id','caregiver_fname','caregiver_lname','caregiver_phone','caregiver_mob'));
                 return view('vnas_users.care', compact('vnas_users'));
             }
             else if ( $isPatient != "" ) 
             {
-                $vnas_users = Vnas_record::where( 'patient_id' , '=' , $isPatient )->distinct()->get( array('patient_id','patient_fname','patient_lname','patient_address','patient_phone','patient_email'));
+                $vnas_users = Vnas_record::where( 'patient_id' , '=' , $isPatient )->distinct()
+                    ->take(1)
+                    ->get( array('patient_id','patient_fname','patient_lname','patient_address','patient_phone','patient_email'));
                 return view('vnas_users.index', compact('vnas_users'));
             }
             //If admin here, go ahead and show the list of patients
