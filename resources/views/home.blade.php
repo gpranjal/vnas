fc4bb8c08c03eaeec15d1dd1a1d02353b49403a0
fmohammed1022 <fmohammed@unomaha.edu>
2016-03-04 20:10:03 -0600

    fixing width issues

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
 <!-- <img src="{{ asset('img/back_arrow.png') }}" align="left" height="40" width="40"> -->
-    <div class="col-md-8 col-md-offset-2" >
+    <div class="col-md-12" >
         <div class="panel panel-default">
             <div class="panel-heading"> <!-- This div has the orange color for the VNA-->
             <h4>Home</h4>
