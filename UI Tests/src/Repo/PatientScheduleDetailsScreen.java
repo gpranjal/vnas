a8cdd3214c364594405c04f1a8b6050789c4743f
josephforsythe <joseph.forsythe@gmail.com>
2016-03-05 03:32:45 -0600

    finished binding items available. Still need to create bindings for FAQ screen.

---
+++ UI Tests/src/Repo/PatientScheduleDetailsScreen.java
@@ -0,0 +1,43 @@
+package Repo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PatientScheduleDetailsScreen {
+	public static String getURL() {
+		return BaseScreen.getURL() + getPath();
+	}
+	
+	public static String getPath() {
+		return "/vnas_records/caregiver";
+	}
+	
+	public static WebElement getEmailButton(WebDriver driver) {
+		return driver.findElement(By.name("mailtoButton"));
+	}
+	
+	public static WebElement getPhoneButton(WebDriver driver) {
+		return driver.findElement(By.name("callButton"));
+	}
+	
+	public static WebElement getIdLabel(WebDriver driver) {
+		return driver.findElement(By.name("idText1"));
+	}
+	
+	public static WebElement getNameLabel(WebDriver driver) {
+		return driver.findElement(By.name("nameText1"));
+	}
+	
+	public static WebElement getAddressLabel(WebDriver driver) {
+		return driver.findElement(By.name("addressText1"));
+	}
+	
+	public static WebElement getPhoneLabel(WebDriver driver) {
+		return driver.findElement(By.name("phoneText1"));
+	}
+	
+	public static WebElement getCommentsLabel(WebDriver driver) {
+		return driver.findElement(By.name("commentsText1"));
+	}
+}
