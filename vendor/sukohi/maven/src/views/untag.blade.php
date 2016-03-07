43565b922612528a545e879ddd27515793d8c48d
josephforsythe <joseph.forsythe@gmail.com>
2016-03-06 21:52:26 -0600

    centered the FAQ screen.

--- vendor/sukohi/maven/src/views/untag.blade.php
+++ vendor/sukohi/maven/src/views/untag.blade.php
@@ -3,55 +3,57 @@
 @section('content')
 
 <div class="container">
-    <div class="col-md-8 col-md-offset-1">
-        <div class="panel panel-default">
-            <div class="panel-heading"> <!-- This div has the orange color for the VNA-->
-                <h4>FAQ</h4>
-            </div>
-            <br />
-            <div class="panel-body">
-                <img src="{{ asset('img/brandmark_main.png') }}" class="img-responsive" alt="VNA logo">
-            </div>
-            <br />
-            <div id="FAQ">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading"> <!-- This div has the orange color for the VNA-->
+					<h4>FAQ</h4>
+				</div>
+				<br />
+				<div class="panel-body">
+					<img src="{{ asset('img/brandmark_main.png') }}" class="img-responsive" alt="VNA logo">
+				</div>
+				<br />
+				<div id="FAQ">
 
 
-<!-- HTML for SEARCH BAR  -->
-    <div id="tfheader">
-        <form name="tstingForm" id="tfnewsearch" method="post" action="{{ url('/faq/search') }}">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
-                <input type="text" id="tfq" class="tftextinput2" name="keyword" value="{{ $keyword }}" autocomplete="off" size="21" maxlength="120" placeholder="Search FAQs"><input type="submit" value=">>" class="tfbutton2">
-        </form>
-        <div class="tfclear"></div>
-    </div>
+					<!-- HTML for SEARCH BAR  -->
+					<div id="tfheader">
+						<form name="tstingForm" id="tfnewsearch" method="post" action="{{ url('/faq/search') }}">
+							<input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+							<input type="text" id="tfq" class="tftextinput2" name="keyword" value="{{ $keyword }}" autocomplete="off" size="21" maxlength="120" placeholder="Search FAQs"><input type="submit" value=">>" class="tfbutton2">
+						</form>
+						<div class="tfclear"></div>
+					</div>
 
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                    <th>Order</th>
-                     <th><nobr>{{ trans('Questions & Answers') }}</nobr></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($faqs as $index => $faq)
-                        <tr>
-                            <td>{!! $faq->sort_number !!}</td>
-                            <td>
-                                <div class="text-bold">{!! $faq->question !!}</div>
-                                <br>
-                                {!! $faq->answer !!}
-                            </td>
-                            <td class="text-center">{!! $faq->draft_flag_icon !!}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            </div>
-            
-            <div class="text-center">
-                {!! $faqs->render() !!}
-            </div>
-        </div>
-    </div>
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Order</th>
+								<th><nobr>{{ trans('Questions & Answers') }}</nobr></th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($faqs as $index => $faq)
+							<tr>
+								<td>{!! $faq->sort_number !!}</td>
+								<td>
+									<div class="text-bold">{!! $faq->question !!}</div>
+									<br>
+									{!! $faq->answer !!}
+								</td>
+								<td class="text-center">{!! $faq->draft_flag_icon !!}</td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+				
+				<div class="text-center">
+					{!! $faqs->render() !!}
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
