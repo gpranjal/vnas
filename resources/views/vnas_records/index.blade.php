cbccd80eb0a53bf17f07e2eba0b2fa1611875f4d
Zach Heath <zheath@unomaha.edu>
2016-03-08 21:23:27 -0600

    Zach - More branding fixes

--- resources/views/vnas_records/index.blade.php
+++ resources/views/vnas_records/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid text-center">
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
 			<div class="panel panel-default">
 				<div class="panel-heading"> 
