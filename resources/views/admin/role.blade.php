aef849b6539b850b3aecfbf69d1e381d286360a2
mayank sawala <mayanksawala@gmail.com>
2016-03-11 16:06:44 -0600

    quick fix 2

--- resources/views/admin/role.blade.php
+++ resources/views/admin/role.blade.php
@@ -1,6 +1,6 @@
 
-    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 <style>
     .search{
         background-position: -160px -112px;
