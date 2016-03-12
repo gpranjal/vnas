c946d8262683bfab5885cf49689b84a65ac5a421
mayank sawala <mayanksawala@gmail.com>
2016-03-12 12:36:33 -0600

    secured the role management

--- resources/views/admin/role.blade.php
+++ resources/views/admin/role.blade.php
@@ -7,7 +7,7 @@
 
 
         $(function() {
-            $( "#patient_search" ).autocomplete({
+            $( "#patient_autocomplete" ).autocomplete({
                 source: function (request, response) {
                     $.getJSON("/search_patient", function (data) {
                         response($.map(data, function (value, key) {
@@ -21,15 +21,15 @@
                 minLength: 2,
                 delay: 500,
                 select: function(event, ui) {
-
-
-
-
-
+                    var my_value = ui.item.value;
+                    console.log(my_value);
+                    $('#patient_fetch').text(my_value);
+                    $('#patient_search').val(my_value);
+                    ui.item.value = '';
 //
                 }})
 
-            $( "#caregiver_search" ).autocomplete({
+            $( "#caregiver_autocomplete" ).autocomplete({
                 source: function (request, response) {
                     $.getJSON("/search_caregiver", function (data) {
                         response($.map(data, function (value, key) {
@@ -43,11 +43,11 @@
                 minLength: 2,
                 delay: 500,
                 select: function(event, ui) {
-
-
-
-
-
+                    var my_caregiver = ui.item.value;
+                    console.log(my_caregiver);
+                    $('#caregiver_fetch').text(my_caregiver);
+                    $('#caregiver_search').val(my_caregiver);
+                    ui.item.value = '';
 //
                 }})
         });
@@ -68,14 +68,16 @@
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <table class="table" style="width:100%">
         <tr>
-            <td>Patient ID: {{$role_id->patient_role}}</td>
-            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="patient_search" name="patient_search" value="{{$role_id->patient_role}}"></td>
-
+            <td>Patient ID: </td>
+            <td><label id="patient_fetch">{{$role_id->patient_role}}</label></td>
+            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="patient_autocomplete" name="patient_autocomplete"></td>
+            <input type="hidden" id="patient_search" name="patient_search" value="{{$role_id->patient_role}}">
         </tr>
         <tr>
-            <td>Caregiver ID: {{$role_id->caregiver_role}}</td>
-            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="caregiver_search" name="caregiver_search" value="{{$role_id->caregiver_role}}"></td>
-
+            <td>Caregiver ID: </td>
+            <td><label id="caregiver_fetch">{{$role_id->caregiver_role}}</label></td>
+            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="caregiver_autocomplete" name="caregiver_autocomplete"></td>
+            <input type="hidden" id="caregiver_search" name="caregiver_search" value="{{$role_id->caregiver_role}}">
         </tr>
     </table>
     <input type="submit" value="submit">
