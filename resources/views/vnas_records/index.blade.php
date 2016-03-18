d3e6984a18e2afe331d1ad21acb24598e40e2e4a
Zach Heath <zheath@unomaha.edu>
2016-03-18 13:20:27 -0500

    Zach - Removing sch id

--- resources/views/vnas_records/index.blade.php
+++ resources/views/vnas_records/index.blade.php
@@ -20,7 +20,6 @@
 				<table class="table table-hover text-left">
 					<thead>
 						<tr>
-							<th>ID</th>
 							<th>Title</th>
 							<th>Date</th>
 							<th>Time</th>
@@ -32,11 +31,10 @@
 						<?php $count = 1 ?>
 						@foreach ($Vnas_records as $Vnas_record)
 						<tr name="{{'rowLink' . $count}}" class='whole-row-click click_row' data-href='{{ action( $nextCntl , [$Vnas_record->id]) }}'>
-							<td name="{{'idText' . $count}}">{{ $Vnas_record->id }}</td>
 							<td name="{{'titleText' . $count}}">{{ $Vnas_record->ap_title }}</td>
 							<td name="{{'dateText' . $count}}">{{ $Vnas_record->ap_date->format("m/d/y") }}</td>
 							<td name="{{'timeText' . $count}}">{{ date( 'H:i' , strtotime( $Vnas_record->ap_date->format("m/d/y") . ' ' . $Vnas_record->ap_time ) ) }}</td>
-							<td name="{{'nameText' . $count}}">{{ $Vnas_record->caregiver_fname  }} {{ $Vnas_record->caregiver_lname }}</td>
+							<td name="{{'nameText' . $count}}">{{ $Vnas_record->caregiver_fname  }} {{ $Vnas_record->caregiver_lname[0] }}</td>
 						</tr>
 						<?php $count=$count+1 ?>
 						@endforeach
