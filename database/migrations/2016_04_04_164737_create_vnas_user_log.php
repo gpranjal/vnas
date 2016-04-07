2e6c628464a208d4c783ff5f059f3b52c5972883
Pranjal Gupta <gpranjal@gmail.com>
2016-04-07 00:56:31 -0500

    Fixed: All migrations, Indexes, Response time. Removed all extra migration scripts. Added default values with 'roles' to FAQs. ETL script will automatically assign Caregiver_Ind value = 1 to patient and client roles in general ref table. Works with the latest ETL scripts.

--- database/migrations/2016_04_04_164737_create_vnas_user_log.php
+++ database/migrations/2016_04_04_164737_create_vnas_user_log.php
@@ -14,15 +14,15 @@ class CreateVnasUserLog extends Migration {
 	{
 		Schema::create('VNAS_USER_LOG', function(Blueprint $table)
 		{
-			$table->string('EMAIL',80);
-			$table->string('FIRST_NAME',40);
-			$table->string('LAST_NAME',40);
-			$table->string('PASSWORD',60);
-			$table->string('TOKEN',60);
-			$table->integer('FAILED_ATTEMPT_CNT');
-			$table->date('LOGIN_TSP');
+			$table->string('EMAIL',80)->nullable();
+			$table->string('FIRST_NAME',40)->nullable();
+			$table->string('LAST_NAME',40)->nullable();
+			$table->string('PASSWORD',60)->nullable();
+			$table->string('TOKEN',60)->nullable();
+			$table->integer('FAILED_ATTEMPT_CNT')->nullable();
+			$table->date('LOGIN_TSP')->nullable();
 			$table->string('USER_ID',20);
-			$table->string('CREATED_BY',100);
+			$table->string('CREATED_BY',100)->default('app_user');
 			$table->timestamp('CREATED_DATE');
 			$table->integer('USER_SK');
 			$table->increments('USER_LOG_SK')->unique();
