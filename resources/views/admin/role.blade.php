5d2b6cda9f2fd4db7f6297d2ed1fe4a1577bc166
mayank sawala <mayanksawala@gmail.com>
2016-04-09 11:12:49 -0500

    changed looks

--- resources/views/admin/role.blade.php
+++ resources/views/admin/role.blade.php
@@ -66,25 +66,33 @@
     }
 </style>
 
-<label style="font-size: 20px">{{$role_array['name']}}</label>
+<label style="font-size: 20px">Username: {{$role_array['name']}}</label><br><br>
 <form id="assign_roles" class="form-horizontal" role="form" method="POST" action="{{url('/role_update')}}/{{$role_array['id']}}">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <table class="table" style="width:100%">
         <tr>
+            <th>Type</th>
+            <th>ID Attached</th>
+            <th>Search (Patient/Caregiver)</th>
+            <th>Remove</th>
+        </tr>
+        <tr>
 
             <td>Patient ID: </td>
             <td><label id="patient_fetch">{{$role_array['client']}}</label></td>
-            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="patient_autocomplete" name="patient_autocomplete"><a href="/remove/patient_role/{{$role_array['id']}}"> Remove Patient Role</a></td>
+            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="patient_autocomplete" name="patient_autocomplete"></td>
+            <td><a href="/remove/patient_role/{{$role_array['id']}}"> Remove Patient Role</a></td>
             <input type="hidden" id="patient_search" name="patient_search" value="">
         </tr>
         <tr>
             <td>Caregiver ID: </td>
             <td><label id="caregiver_fetch">{{$role_array['caregiver']}}</label></td>
-            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="caregiver_autocomplete" name="caregiver_autocomplete"><a href="/remove/caregiver_role/{{$role_array['id']}}"> Remove Caregiver Role</a></td>
+            <td><input class="search ui-autocomplete-input" type="text" size="25" maxlength="50" id="caregiver_autocomplete" name="caregiver_autocomplete"></td>
+            <td><a href="/remove/caregiver_role/{{$role_array['id']}}"> Remove Caregiver Role</a></td>
             <input type="hidden" id="caregiver_search" name="caregiver_search" value="">
         </tr>
     </table>
-    <input class="btn btn-primary" role="button" name="btnSubmit" type="Submit" value="submit" />
+    <input class="btn btn-primary" role="button" name="btnSubmit" type="Submit" value="Submit" />
     <a name="btnCancel" class="btn btn-primary" role="button" href="{{url('/manage')}}">Cancel</a>
 </form>
     @if(isset($_SESSION['role_msg']))
