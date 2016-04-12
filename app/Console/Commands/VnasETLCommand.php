7250ef445907b22e9202c624c709cc4c778fe38d
Zach Heath <zheath@unomaha.edu>
2016-04-12 00:04:27 -0500

    Zach - testing cron

---
+++ app/Console/Commands/VnasETLCommand.php
@@ -0,0 +1,70 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class VnasETLCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	/* mysql app < ./VNSApplicationDatabaseETLLoadScript.sql */
+	/* mysql vnas < ./VNSApplicationDatabaseETLLoadScript.sql */
+	protected $name = "exec:etl";
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Execute VNA ETL Script on server.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		//$this->call('mysql vnas < ./VNSApplicationDatabaseETLLoadScript.sql');
+		$this->exec('mysql vnas < ./VNSApplicationDatabaseETLLoadScript.sql');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+// 	protected function getArguments()
+// 	{
+// 		return [
+// 			['example', InputArgument::REQUIRED, 'An example argument.'],
+// 		];
+// 	}
+
+// 	/**
+// 	 * Get the console command options.
+// 	 *
+// 	 * @return array
+// 	 */
+// 	protected function getOptions()
+// 	{
+// 		return [
+// 			['example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
+// 		];
+// 	}
+
+}
