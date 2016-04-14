626660951be56fe75cff64c28760609b1b7f3936
fmohammed1022 <fmohammed@unomaha.edu>
2016-04-14 12:26:37 -0500

    Adding date range (F/C & H records) to patient schedule view.

--- resources/views/vnas_records/index.blade.php
+++ resources/views/vnas_records/index.blade.php
@@ -22,6 +22,25 @@
 						{{ $myMessage }}
 					</div>
 				@else
+
+				<div id="dateRangeFilterOuter">
+					<form name="dateRangeForm" id="dateRangeCheck" action="{{ url('vnas_records/') }}" method="post">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<fieldset class="form-group" style="width: 50%; float:none; margin: 0 auto;">
+							<label>Select Date Range:</label>
+							<!-- <?php echo ($myRole); echo(" -- I am in ".$myRangeValue." view"); ?> -->
+							<select name="dateRangeFilterInner" id="dateRangeFilterInner" class="form-control">
+
+								@foreach ($dateRange as $mydateRangeVal)
+
+									<option value="{{ $mydateRangeVal }}" @if( $mydateRangeVal == $myRangeValue ) selected="selected" @endif>{{ $mydateRangeVal }}</option>
+								@endforeach
+
+							</select>
+						</fieldset>
+					</form>
+				</div>
+
 				<table class="table table-hover text-left">
 					<thead>
 						<tr>
@@ -67,9 +86,24 @@
 
 <script language="javascript">
 	jQuery(document).ready(function($) {
-	    $(".whole-row-click").click(function() {
-	        window.document.location = $(this).data("href");
-	    });
+		$(".whole-row-click").click(function() {
+			window.document.location = $(this).data("href");
+		});
+
+		$('#dateRangeFilterInner').on('change', function(e){
+			var select = $(this), form = $("#dateRangeCheck"), currPath = form.attr('action'), newPath = "";
+
+			if( $( select ).val() != "Current")
+			{
+				newPath = "/filter/All/"+ $( select ).val();
+				form.attr('action', form.attr('action') + newPath );
+				form.submit();
+			}
+			else
+			{
+				window.location.href="{{ url('vnas_records/') }}";
+			}
+		});
 	});
 </script>
 
